433a04344e0d9d86cfe0b965b1612f8d0aff43b3
waradis <waradis.kam@dome.tu.ac.th>
2023-10-29 00:15:19 +0700

    link login with form page

--- src/main/java/com/tu/ac/th/services/ServicesApplication.java
+++ src/main/java/com/tu/ac/th/services/ServicesApplication.java
@@ -2,8 +2,10 @@ package com.tu.ac.th.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class ServicesApplication {
 
 	public static void main(String[] args) {
