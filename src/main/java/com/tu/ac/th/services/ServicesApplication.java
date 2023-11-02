6f647a10368c90722c92ba1ee5a189486e726175
waradis <waradis.kam@dome.tu.ac.th>
2023-11-02 21:49:05 +0700

    modified:   src/main/java/com/tu/ac/th/services/ServicesApplication.java modified:   target/classes/com/tu/ac/th/services/ServicesApplication.class

--- src/main/java/com/tu/ac/th/services/ServicesApplication.java
+++ src/main/java/com/tu/ac/th/services/ServicesApplication.java
@@ -2,9 +2,8 @@ package com.tu.ac.th.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 
 public class ServicesApplication {
 
