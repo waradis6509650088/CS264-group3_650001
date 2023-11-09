588a49322046ce0c6d0efc75a8a05404a85d80ac
waradis <waradis.kam@dome.tu.ac.th>
2023-11-09 22:53:47 +0700

    modified:   src/main/java/com/tu/ac/th/services/Controllers/AuthController.java 	modified:   src/main/resources/static/script/script.js 	modified:   target/classes/com/tu/ac/th/services/Controllers/AuthController.class 	modified:   target/classes/com/tu/ac/th/services/Repository/FormRepository.class 	modified:   target/classes/com/tu/ac/th/services/Repository/JdbcRepository.class 	modified:   target/classes/static/script/script.js

--- src/main/java/com/tu/ac/th/services/Controllers/AuthController.java
+++ src/main/java/com/tu/ac/th/services/Controllers/AuthController.java
@@ -1,5 +1,13 @@
 package com.tu.ac.th.services.Controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
     
+    @PostMapping("/checkAuth")
+    public int checkAuth(String authJsonString){
+        return 0;
+    }
 }
--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,7 +1,12 @@
-const apiForm = document.getElementById('apiForm');
-const apiResponse = document.getElementById('apiResponse');
 
-function login(){
+
+
+//login function
+//ต้อง pass จาก หน้า login, เดี๋ยวให้ กญ ใส่ฟังชั่นในฟุ่มเข้าสู่ระบบ
+//  vvส่งพร้อมตอนเรียก func
+// const apiForm = document.getElementById('apiForm');
+// const apiResponse = document.getElementById('apiResponse');
+function login(apiForm){
     // รับข้อมูลจากฟอร์ม
     formData = new FormData(apiForm);
 
@@ -11,35 +16,35 @@ function login(){
     jsonData[key] = value
     });
 
-// ส่งข้อมูลไปยัง API
-sendDataToAPI(jsonData);
+    // ส่งข้อมูลไปยัง API
+    sendDataToAPI(jsonData);
 }
 
 function sendDataToAPI(data) {
-// URL ของ API
-const apiUrl = 'https://restapi.tu.ac.th/api/v1/auth/Ad/verify';
+    // URL ของ API
+    const apiUrl = 'https://restapi.tu.ac.th/api/v1/auth/Ad/verify';
 
-// สร้างคำขอ POST โดยใช้ Fetch API
-fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json',
-    'Application-Key': 'TUe2e59e8f883232ef8d55e1df294429a53ebe2b674e71f1b3a2dfd91d4f978aa699bb71061f3ce8da1d35f4010396f273', // คีย์แอปพลิเคชัน
-    },
-    body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(responseData => {
-    if (responseData.status && responseData.type == "student") {
-    // ทำการนำทางไปยังหน้าอื่น ๆ หากมีข้อมูลถูกต้อง
-    window.location.href = 'form.html';
-    } else {
-    // ไม่พบข้อมูลหรือข้อมูลไม่ถูกต้อง
-    apiResponse.innerHTML = 'ไม่พบข้อมูลหรือข้อมูลไม่ถูกต้อง';
-    }
-})
-.catch(error => {
-    console.error('เกิดข้อผิดพลาดในการเรียกใช้ API:', error);
-    apiResponse.innerHTML = 'เกิดข้อผิดพลาดในการเรียกใช้ API';
-});
+    // สร้างคำขอ POST โดยใช้ Fetch API
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        'Application-Key': 'TUe2e59e8f883232ef8d55e1df294429a53ebe2b674e71f1b3a2dfd91d4f978aa699bb71061f3ce8da1d35f4010396f273', // คีย์แอปพลิเคชัน
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        if (responseData.status && responseData.type == "student") {
+        // ทำการนำทางไปยังหน้าอื่น ๆ หากมีข้อมูลถูกต้อง
+        window.location.href = 'form.html';
+        } else {
+        // ไม่พบข้อมูลหรือข้อมูลไม่ถูกต้อง
+        apiResponse.innerHTML = 'ไม่พบข้อมูลหรือข้อมูลไม่ถูกต้อง';
+        }
+    })
+    .catch(error => {
+        console.error('เกิดข้อผิดพลาดในการเรียกใช้ API:', error);
+        apiResponse.innerHTML = 'เกิดข้อผิดพลาดในการเรียกใช้ API';
+    });
 }
