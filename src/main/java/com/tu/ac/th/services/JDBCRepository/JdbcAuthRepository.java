507f6f2a29d7ea33d132f874b21ac00643ad3882
waradis6509650088 <waradis.kam@dome.tu.ac.th>
2023-11-15 19:59:22 +0700

    database connection

--- src/main/java/com/tu/ac/th/services/JDBCRepository/JdbcAuthRepository.java
+++ src/main/java/com/tu/ac/th/services/JDBCRepository/JdbcAuthRepository.java
@@ -1,12 +1,12 @@
 package com.tu.ac.th.services.JDBCRepository;
 
+import com.tu.ac.th.services.JdbcRowMapper.AuthMapper;
 import com.tu.ac.th.services.Models.Auth;
 import com.tu.ac.th.services.Repository.AuthRepository;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -30,10 +30,8 @@ public class JdbcAuthRepository implements AuthRepository {
     public List<Auth> findById(String id) {
         try{
             String sql = "select * from authTable WHERE studentId = ?";
-            List<Auth> auth = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Auth.class), id);
-            // Auth auth = jdbcTemplate.queryForObject(sql, , id);
+            List<Auth> auth =  jdbcTemplate.query(sql, new AuthMapper(), id);
             return auth;
-            //(Auth) jdbcTemplate.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper(Auth.class));
         }catch(Exception e){
             return null;
         }
